bf3fd05a51f65afaabc7cbeaf0e30e6bf5ee1c61
Danil Beltyukov <beltyukov.danil.evgenevich@gmail.com>
2024-12-29 04:08:35 +0300

    Created AuthorService

---
+++ TestTask/Services/Implementations/AuthorService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations;
+
+public class AuthorService : IAuthorService
+{
+    private readonly ApplicationDbContext _context;
+    
+    public AuthorService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Author> GetAuthor()
+    {
+        return await _context.Authors
+            .OrderByDescending(x => x.Books.Max(b => b.Title.Length))
+            .FirstOrDefaultAsync();
+    }
+
+    public async Task<List<Author>> GetAuthors()
+    {
+        const int minYear = 2015;
+        return await _context.Authors
+            .Where(a =>
+                a.Books.Count(b => b.PublishDate.Year > minYear) > 0 &&
+                a.Books.Count % 2 == 0)
+            .ToListAsync();
+    }
+}
